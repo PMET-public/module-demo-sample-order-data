29de4b694f939de32144de179af43e28636cba47
Jeffrey Britts <jbritts@magento.com>
2018-01-18 10:14:17 -0500

    uprade data to fix missind base currency in orders

---
+++ Setup/UpgradeData.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Copyright © 2015 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+namespace MagentoEse\DemoSampleOrderData\Setup;
+
+use Magento\Framework\Setup\UpgradeDataInterface;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+
+class UpgradeData implements UpgradeDataInterface
+{
+    public $_resourceConfig;
+    /**
+     * @var \Magento\Framework\App\Config\ScopeConfigInterface
+     */
+    protected $scopeConfig;
+    private $encrypted;
+
+    public function __construct(
+        \Magento\Framework\App\ResourceConnection $resourceConnection
+    ) {
+        $this->resourceConnection = $resourceConnection;
+
+    }
+
+    public function upgrade( ModuleDataSetupInterface $setup, ModuleContextInterface $context )
+    {
+        if (version_compare($context->getVersion(), '0.0.2', '<=')) {
+            //add base currency code to existing orders for advanced Reporting
+            $connection = $this->resourceConnection->getConnection();
+            $orderTableName = $connection->getTableName('sales_order');
+            $sql = "update " . $orderTableName . " set base_currency_code =  'USD' where base_currency_code is NULL";
+            $connection->query($sql);
+        }
+    }
+
+}
