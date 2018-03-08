cd4aee7c360e431bf709eb9c4b7c033bb54d6304
Jeff Britts <jbritts@magento.com>
2018-03-08 09:44:27 -0500

    cloud test

--- Model/CreditMemos.php
+++ Model/CreditMemos.php
@@ -5,9 +5,10 @@ namespace MagentoEse\DemoSampleOrderData\Model;
 
 use Magento\Sales\Api\OrderRepositoryInterface as OrderRepository;
 use Magento\Sales\Model\Order\CreditmemoFactory as CreditMemoFactory;
-use \Magento\Sales\Api\RefundInvoiceInterface;
+use Magento\Sales\Api\RefundInvoiceInterface;
 use Magento\Sales\Model\Order;
 use Magento\Sales\Model\Order\Creditmemo;
+use Magento\Sales\Api\CreditmemoItemRepositoryInterface as CreditMemoItemRepository;
 
 class CreditMemos
 {
@@ -17,27 +18,41 @@ class CreditMemos
     /** @var CreditMemo  */
     private $creditMemoFactory;
 
+    /** @var  CreditMemoItemRepository */
+    private $creditMemoItemRepository;
+
     /**
      * CreditMemos constructor.
      * @param OrderRepository $orderRepository
-     * @param CreditMemo $creditMemo
+     * @param CreditMemoFactory $creditMemoFactory
+     * @param RefundInvoiceInterface $refundInvoice
+     * @param CreditMemoItemRepository $creditMemoItemRepository
      */
-    public function __construct(OrderRepository $orderRepository, CreditMemoFactory $creditMemoFactory, RefundInvoiceInterface $refundInvoice)
+    public function __construct(OrderRepository $orderRepository, CreditMemoFactory $creditMemoFactory,
+                                RefundInvoiceInterface $refundInvoice, CreditMemoItemRepository $creditMemoItemRepository)
     {
         $this->orderRepository = $orderRepository;
         $this->creditMemoFactory = $creditMemoFactory;
         $this->refundInvoice = $refundInvoice;
+        $this->creditMemoItemRepository = $creditMemoItemRepository;
     }
 
     public function createRefunds(){
 
         /** @var Order $order */
-        $order = $this->orderRepository->get(2);
+        $order = $this->orderRepository->get(3996);
         /** @var CreditMemo $creditMemo */
         $creditMemo = $this->creditMemoFactory->createByOrder($order);
-        $creditMemo->setBaseAdjustment(20);
-        $creditMemo->setBaseAdjustmentPositive(20);
-        $creditMemo->collectTotals();
+        //$creditMemo->setBaseAdjustment(10);
+        //$creditMemo->setBaseAdjustmentPositive(10);
+        /** @var \Magento\Sales\Api\Data\CreditmemoItemInterface $items */
+        $items = $creditMemo->getItems();
+        foreach($items as $item){
+            $item->setQty(1);
+            break;
+        }
+        $creditMemo->setItems($items);
+        //$creditMemo->collectTotals();
         $creditMemo->setState(2);
         $creditMemo->save();
     }
